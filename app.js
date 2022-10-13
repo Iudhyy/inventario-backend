a4e48f463d4b1533fa6fc4847529fdee31ce5b54
Iudhyy <infornet@sistemafieto.rede>
2022-10-13 20:29:56 -0300

    teste

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const alunos=[
+    {nome:"Iudhy",endereco:"rua jm-35",idade:"18"},
+    {nome:"João",endereco:"rua 8",idade:"20"},
+    {nome:"Santos",endereco:"rua 12",idade:"22"}
+]
+const cadastro={
+    nome:"CARLOS",
+    endereco:"rua sta cruz",
+    idade:39,
+    cursos:["informatica","pedreiro"],
+    conjuge:[
+        {nome:"Maria",idade:20,sexo:"F"}
+    ]
+}
 const usuarios =[{
     nome:"Pedro",idade:20,
     nome:"joão",idade:50,
