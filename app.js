9ac6519035e100ce99ca40ec4f67167ece3aecae
Iudhy_ <63312967+Iudhyy@users.noreply.github.com>
2022-10-27 20:47:43 -0300

    validação do código

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 const rotaUsuarios = require('./routes/rotasUsuarios');
+const rotaEmpresas = require('./routes/rotaEmpresas');
 
 
 app.use('/usuario',rotaUsuarios);
+app.use('/empresa',rotaEmpresas);
+
   
 module.exports = app;
