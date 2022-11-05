07d9f2f2e1b271902b91092c2464fc065bf5701f
Iudhy_ <63312967+Iudhyy@users.noreply.github.com>
2022-11-04 21:08:10 -0300

    corrigindo rotas

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ const rotaUsuarios = require('./routes/rotasUsuarios');
 const rotaEmpresas = require('./routes/rotaEmpresas');
 const rotaPatrimonios = require('./routes/rotaPatrimonios');
 const rotaSetores = require('./routes/rotaSetores');
+const rotaLotacao = require('./routes/rotaLotacao');
 
 
 app.use('/usuario',rotaUsuarios);
 app.use('/empresa',rotaEmpresas);
 app.use('/patrimonio',rotaPatrimonios);
 app.use('/setor',rotaSetores);
+app.use('/lotacao',rotaLotacao);
 
 
 
